a92522f0600267b64bc58d829a9d93b0e8e5fedd
Daniel <136742291+danlima133@users.noreply.github.com>
2024-10-28 13:49:54 +0000

    feat: init with phaser

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,11 +1,35 @@
 import { Game, AUTO } from 'phaser'
 
+let gameSize = {
+    width: 384,
+    height: 216
+}
+
 let config = {
     type: AUTO,
-    width: 384,
-    height: 216,
+    width: gameSize.width,
+    height: gameSize.height,
     parent: "game",
-    backgroundColor: "#0377fc"
+    backgroundColor: "#0377fc",
+    scene: {
+        preload: preload,
+        create: create
+    }
 }
 
 let game = new Game(config)
+
+function preload() {
+    this.load.image("bg", "assets/background1.png")
+}
+
+function create() {
+    const bg = this.add.image(gameSize.width/2, gameSize.height/2, "bg")
+    const title = this.add.text(gameSize.width/2, gameSize.height/2 - 50, "Memory Game", {
+        fontFamily: 'sans serif',
+        color: "#ffff",
+        fontSize: '30px',
+        resolution: 1
+    })
+    title.setOrigin(0.5, 0.5)
+}
